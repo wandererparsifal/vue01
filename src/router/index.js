bc22ea68970a3b2b4e4c0dd136aa888018018bfd
wandererparsifal <wandererparsifal@gmail.com>
2017-11-10 16:36:54 +0800

    详情页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/pages/HelloWorld';
+import GoodsDetail from '@/pages/GoodsDetail';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld,
+    },
+    {
+      path: '/detail',
+      name: 'GoodsDetail',
+      component: GoodsDetail,
     },
   ],
 });
