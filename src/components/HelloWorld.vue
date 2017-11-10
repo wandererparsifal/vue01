b23f2e1cebb937489517771103e76fa4fb8855df
wandererparsifal <wandererparsifal@gmail.com>
2017-11-10 15:16:58 +0800

    添加list

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,7 @@
 <template>
   <div class="hello">
     <home-header></home-header>
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <goods-list></goods-list>
     <common-footer></common-footer>
   </div>
 </template>
@@ -25,9 +9,10 @@
 <script>
   import HomeHeader from './HomeHeader';
   import CommonFooter from './CommonFooter';
+  import GoodsList from './GoodsList';
 
   export default {
-    components: { CommonFooter, HomeHeader },
+    components: { GoodsList, CommonFooter, HomeHeader },
     name: 'HelloWorld',
     data() {
       return {
